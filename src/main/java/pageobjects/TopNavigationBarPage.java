74479d7c8e1ead3051855dc2f0a4b471a0143bd8
Acer3Robert <robert.qwerty1989@gmail.com>
2025-06-11 13:37:16 +0200

    create page, add few buttons and methods

---
+++ src/main/java/pageobjects/TopNavigationBarPage.java
@@ -0,0 +1,42 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class TopNavigationBarPage extends BasePage{
+
+    public TopNavigationBarPage(WebDriver driver){
+        super(driver);
+    }
+
+    @FindBy(xpath = "//header[@id=\"header\"]//input[@name=\"q\" and @type=\"search\"]")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//header[@id=\"header\"]//div[@class=\"h-quick-search-dropdown\"]//button[@id=\"d\"]")
+    private WebElement searchCategoriesDropdownButton;
+
+    @FindBy(xpath = "//div[@class=\"h-controls\"]//button[@data-ask-widget=\"help-center\"]")
+    private WebElement contactButton;
+
+    @FindBy(xpath = "//div[@class=\"h-control h-shopping-lists-control\"]//button")
+    private WebElement shoppingListButton;
+
+    @Override
+    public void waitForPageToBeLoaded() {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-testid=\"login-btn\"]")));
+    }
+
+    public void clickContactButton(){
+        contactButton.click();
+    }
+    public void clickshoppingListButton(){
+        shoppingListButton.click();
+    }
+}
