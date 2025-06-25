0ea3393548bd26962d7d6bf5482903fb91b3f82e
Acer3Robert <robert.qwerty1989@gmail.com>
2025-06-25 15:13:43 +0200

    delete unused imports

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,14 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pageobjects.AcceptCookiesPage;
-
-import java.time.Duration;
 
 public class BaseTest {
     WebDriver driver;
